0e4217fe8bac878743da34dd1035c021058fe170
Pavlo Kataranchuk <pavel83838300@gmail.com>
2023-09-12 11:02:54 +0300

    fixed

--- back-end/routes/AuthRouter.js
+++ back-end/routes/AuthRouter.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const AuthController = require("../controllers/AuthController");
 const authentication = require("../middlewares/authentication");
 
-router.post("/register", AuthController.registerUser);
+router.post("/registration", AuthController.registerUser);
 router.post("/login", AuthController.loginUser);
 router.get("/me", authentication, AuthController.getCurrentUser);
 router.get("/logout", AuthController.getCurrentUser);
